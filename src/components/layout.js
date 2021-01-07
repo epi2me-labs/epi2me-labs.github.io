d83418a0022de44308f2267eb693ae063285e743
Chris Wright <chris.wright@nanoporetech.com>
2021-01-07 10:01:37 +0000

    Update id

--- src/components/layout.js
+++ src/components/layout.js
@@ -5,7 +5,7 @@ import NavBar from "./navBar/navBar";
 import { rhythm, scale } from "../utils/typography"
 import CookieConsent from 'react-cookie-consent';
 
-const HEAP_APPID = '38910241'
+const HEAP_APPID = '3572316639'
 
 
 const Layout = ({ location, title, children }) => {
@@ -58,7 +58,6 @@ const Layout = ({ location, title, children }) => {
         <Helmet>
           <script type="text/javascript">{`
             window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};   
-            heap.load("38910241"); 
           `}</script>
         </Helmet>
 
