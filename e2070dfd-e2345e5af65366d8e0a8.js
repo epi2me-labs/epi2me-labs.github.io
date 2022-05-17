2c43bd45ebc12932a8275a832ee36e251ce61944
Chris Wright <chris.wright@nanoporetech.com>
2022-05-17 17:03:14 +0100

    Updates

---
+++ e2070dfd-e2345e5af65366d8e0a8.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk_elegantstack_gatsby_starter_flexiblog_minimal_v2=self.webpackChunk_elegantstack_gatsby_starter_flexiblog_minimal_v2||[]).push([[492],{1184:function(t,a,e){e.d(a,{OrA:function(){return l}});var n=e(8575);function l(t){return(0,n.w_)({tag:"svg",attr:{viewBox:"0 0 24 24"},child:[{tag:"path",attr:{d:"M16.293 9.293 12 13.586 7.707 9.293l-1.414 1.414L12 16.414l5.707-5.707z"}}]})(t)}}}]);
+//# sourceMappingURL=e2070dfd-e2345e5af65366d8e0a8.js.map
